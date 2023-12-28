a686524e7f5f6d640783698f5d8b08576193baa9
aesoper <weilanzhuan@163.com>
2023-12-28 19:36:08 +0800

    fix: fix cli

--- scripts/build-cli.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-lerna run build \
-  --scope @aesoper/build-cli
--- scripts/build-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-lerna run build \
-  --scope @aesoper/vite-plugin-external \
-  --scope @aesoper/vite-plugin-cssjs
