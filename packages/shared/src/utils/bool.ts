c7d354a34f31462327e3e8fd89ffb539de152dec
aesoper <weilanzhuan@163.com>
2024-01-04 18:20:00 +0800

    feat: add data

--- packages/shared/src/utils/bool.ts
+++ packages/shared/src/utils/bool.ts
@@ -1,3 +1,9 @@
 export type Truthy<T> = T extends false | '' | 0 | null | undefined ? never : T;
 
-export const isTruthy = <T>(value: T): value is Truthy<T> => Boolean(value);
+export const isTruthy = <T>(value: T): value is Truthy<T> => {
+  return Boolean(value);
+};
+
+export type Falsy<T> = T extends false | '' | 0 | null | undefined ? T : never;
+
+export const isFalsy = <T>(value: T): value is Falsy<T> => !isTruthy(value);
